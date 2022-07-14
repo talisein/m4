2eeae0199bdc2bb64dba186bd2a41116d3d9d0a8
Andrew Potter <agpotter@gmail.com>
2022-07-14 16:26:19 -0700

    Add tests

---
+++ checks/m4_test.py
@@ -0,0 +1,41 @@
+import sys
+import subprocess
+
+def main() -> int:
+    """Echo the input arguments to standard output"""
+    m4 = sys.argv[1]
+    inputfile = sys.argv[2]
+    with open(inputfile) as f:
+        lines = f.readlines()
+        expected_out = []
+        data = bytes()
+        for l in lines:
+            if l.startswith('dnl @ expected status: '):
+                expected_code = l[23:].rstrip()
+            if l.startswith('dnl @ extra options: '):
+                args = l[22:].rstrip()
+            if l.startswith('dnl @result{}'):
+                expected_out.append(l[13:])
+            if not l.startswith('dnl @'):
+                data += l.encode()
+        runargs = []
+        runargs.append(m4)
+        runargs.append('-d')
+        for arg in args.split(' '):
+            if len(arg) > 0:
+                runargs.append(arg)
+        runargs.append('-')
+        res = subprocess.run(runargs, input=data, capture_output=True)
+        if res.returncode == 77:
+            return 77
+        if str(res.returncode) != expected_code:
+            print('Unexpected return code: {0}, expected {1}'.format(res.returncode, expected_code))
+            return 1
+        byte_expected = ''.join(expected_out).encode('UTF-8')
+        if byte_expected != res.stdout:
+            print('unexpected output. Expected:\n{0}\nGot:\n{1}'.format(byte_expected, res.stdout))
+            return 1
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())  # next section explains the use of sys.exit
