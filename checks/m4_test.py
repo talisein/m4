5096426d1ad4a9731a337d7bb531c753baef8698
Andrew Potter <agpotter@gmail.com>
2022-07-14 17:03:55 -0700

    assign earlier

--- checks/m4_test.py
+++ checks/m4_test.py
@@ -8,14 +8,17 @@ def main() -> int:
     with open(inputfile) as f:
         lines = f.readlines()
         expected_out = []
+        expected_err = []
         data = bytes()
         for l in lines:
             if l.startswith('dnl @ expected status: '):
-                expected_code = l[23:].rstrip()
+                expected_code = l[len('dnl @ expected status: '):].rstrip()
             if l.startswith('dnl @ extra options: '):
-                args = l[22:].rstrip()
+                args = l[len('dnl @ extra options: '):].rstrip()
             if l.startswith('dnl @result{}'):
-                expected_out.append(l[13:])
+                expected_out.append(l[len('dnl @result{}'):])
+            if l.startswith('dnl @ expected error: '):
+                expected_err.append(l[len('dnl @ expected error: '):])
             if not l.startswith('dnl @'):
                 data += l.encode()
         runargs = []
@@ -25,6 +28,8 @@ def main() -> int:
             if len(arg) > 0:
                 runargs.append(arg)
         runargs.append('-')
+        byte_expected = ''.join(expected_out).encode('UTF-8')
+        byte_expected_err = ''.join(expected_err).encode('UTF-8')
         res = subprocess.run(runargs, input=data, capture_output=True)
         if res.returncode == 77:
             return 77
@@ -34,12 +39,15 @@ def main() -> int:
             if len(res.stderr) > 0:
                 printf('Stderr:\n{0}'.format(res.stderr))
             return 1
-        byte_expected = ''.join(expected_out).encode('UTF-8')
         if byte_expected != res.stdout.replace(b'\r\n', b'\n'):
             print('unexpected output. Expected:\n{0}\nGot:\n{1}'.format(byte_expected, res.stdout.replace(b'\r\n', b'\n')))
             if len(res.stderr) > 0:
                 printf('Stderr:\n{0}'.format(res.stderr))
             return 1
+        if len(byte_expected_err) > 0 and byte_expected_err != b'ignore\n' and byte_expected_err != res.stderr.replace(b'\r\n', b'\n'):
+            print('unexpected error. Expected:\n{0}\nGot:\n{1}'.format(byte_expected_err, res.stderr.replace(b'\r\n', b'\n')))
+            return 1
+
     return 0
 
 if __name__ == '__main__':
