69045489d5382caab0bc4cf6056ba859ef03eb43
Andrew Potter <agpotter@gmail.com>
2022-07-15 09:28:00 -0700

    debug

--- checks/m4_test.py
+++ checks/m4_test.py
@@ -23,7 +23,7 @@ def main() -> int:
                 data += l.encode()
         runargs = []
         runargs.append(m4)
-        runargs.append('-d')
+        runargs.append('-d aeqp')
         for arg in args.split(' '):
             if len(arg) > 0:
                 runargs.append(arg)
