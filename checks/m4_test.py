6a1b0581b120fc443da4d7f65328e1ad97a64606
Andrew Potter <agpotter@gmail.com>
2022-07-15 13:11:43 -0700

    careful with lineendings

--- checks/m4_test.py
+++ checks/m4_test.py
@@ -5,44 +5,44 @@ def main() -> int:
     """Echo the input arguments to standard output"""
     m4 = sys.argv[1]
     inputfile = sys.argv[2]
-    with open(inputfile) as f:
-        lines = f.readlines()
+    with open(inputfile, 'rb') as f:
+        lines = f.read().splitlines(keepends=True)
         expected_out = []
         expected_err = []
         data = bytes()
         for l in lines:
-            if l.startswith('dnl @ expected status: '):
-                expected_code = l[len('dnl @ expected status: '):].rstrip()
-            if l.startswith('dnl @ extra options: '):
-                args = l[len('dnl @ extra options: '):].rstrip()
-            if l.startswith('dnl @result{}'):
+            if l.startswith(b'dnl @ expected status: '):
+                expected_code = int(l[len('dnl @ expected status: '):].rstrip())
+            if l.startswith(b'dnl @ extra options: '):
+                args = l[len('dnl @ extra options: '):].rstrip().decode()
+            if l.startswith(b'dnl @result{}'):
                 expected_out.append(l[len('dnl @result{}'):])
-            if l.startswith('dnl @ expected error: '):
+            if l.startswith(b'dnl @ expected error: '):
                 expected_err.append(l[len('dnl @ expected error: '):])
-            if not l.startswith('dnl @'):
-                data += l.encode()
+            if not l.startswith(b'dnl @'):
+                data += l
         runargs = []
         runargs.append(m4)
-        runargs.append('--debug=aeqp')
+        runargs.append('-d')
         for arg in args.split(' '):
             if len(arg) > 0:
                 runargs.append(arg)
         runargs.append('-')
-        byte_expected = ''.join(expected_out).encode('UTF-8')
-        byte_expected_err = ''.join(expected_err).encode('UTF-8')
+        byte_expected = b''.join(expected_out)
+        byte_expected_err = b''.join(expected_err)
         res = subprocess.run(runargs, input=data, capture_output=True)
         if res.returncode == 77:
             return 77
-        if str(res.returncode) != expected_code:
+        if res.returncode != expected_code:
             print('Unexpected return code: {0}, expected {1}'.format(res.returncode, expected_code))
-            print('Here is the output. Expected:\n{0}\nGot:\n{1}'.format(byte_expected, res.stdout.replace(b'\r\n', b'\n')))
+            print('Here is the output. Expected:\n{0}\nGot:\n{1}'.format(byte_expected, res.stdout))
             if len(res.stderr) > 0:
                 print('Stderr:\n{0}'.format(res.stderr))
             return 1
-        if byte_expected != res.stdout.replace(b'\r\n', b'\n'):
-            print('unexpected output. Expected:\n{0}\nGot:\n{1}'.format(byte_expected, res.stdout.replace(b'\r\n', b'\n')))
+        if byte_expected.splitlines() != res.stdout.splitlines():
+            print('Enexpected output. Expected:\n{0}\nGot:\n{1}'.format(byte_expected, res.stdout))
             if len(res.stderr) > 0:
-                print('Stderr:\n{0}'.format(res.stderr))
+                print('stderr:\n{0}'.format(res.stderr))
             return 1
         if len(byte_expected_err) > 0 and byte_expected_err != b'ignore\n' and byte_expected_err != res.stderr.replace(b'\r\n', b'\n'):
             print('unexpected error. Expected:\n{0}\nGot:\n{1}'.format(byte_expected_err, res.stderr.replace(b'\r\n', b'\n')))
